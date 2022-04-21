c49859b6134976e0d3fae377280b3802f13654a8
arthurariza <tucaariza@gmail.com>
2022-04-21 16:14:15 -0300

    Numbers and String

--- studio_game.rb
+++ studio_game.rb
@@ -1,5 +1,9 @@
-greeting = 'Welcome!'
+name1 = 'larry'
+health1 = 60
 
-3.times do
-  puts greeting
-end
+name2 = 'curly'
+name3 = 'moe'
+
+puts "#{name1}'s health is #{health1 / 9.0}"
+
+puts "Players:\n\t#{name1}\n\t#{name2}\n\t#{name3}"
