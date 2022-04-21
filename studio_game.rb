7cb9f158ade47b8575f2921352cfb8405c120085
arthurariza <tucaariza@gmail.com>
2022-04-21 20:04:43 -0300

    Attributes

--- studio_game.rb
+++ studio_game.rb
@@ -1,11 +1,22 @@
 class Player
+  attr_accessor :name
+  attr_reader :health
+
   def initialize(name, health = 100)
     @name = name.capitalize
     @health = health
   end
 
   def to_s
-    "I'm #{@name} with a health of #{@health}"
+    "I'm #{@name} with a health of #{@health} and a score of #{score}"
+  end
+
+  def name=(name)
+    @name = name.capitalize
+  end
+
+  def score
+    @health + @name.size
   end
 
   def blam
@@ -23,11 +34,8 @@ player1 = Player.new('larry', 60)
 player2 = Player.new('curly', 125)
 player3 = Player.new('moe')
 
+player1.name = 'lawrence'
+
 puts player1
 puts player2
 puts player3
-
-player2.blam
-puts player2
-player2.w00t
-puts player2
